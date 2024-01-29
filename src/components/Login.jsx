1f1f0412730a11ec527a48fc0236c621991e279e
Roberto <roberto.quadraccia90@gmail.com>
2024-01-29 17:48:07 +0100

    Validating input upon form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
-
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    email.current.value = ''; // not recommended
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false)
+    // email.current.value = ''; // not recommended
   };
 
   return (
@@ -22,22 +29,15 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            ref={email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            ref={password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
