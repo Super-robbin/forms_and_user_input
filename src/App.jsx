a18ca19769866942fe4f79c5be7ddc1f60106dfc
Roberto <roberto.quadraccia90@gmail.com>
2024-01-30 13:51:42 +0100

    Custom Input component created and password validation implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header.jsx';
 import Signup from './components/Signup.jsx';
-import Login from './components/Login.jsx';
+// import Login from './components/Login.jsx';
+import Login from './components/StateLogin.jsx';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Header />
       <main>
         {/* <Signup /> */}
-        <Signup />
+        <Login />
       </main>
     </>
   );
