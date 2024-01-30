e49f63d58129829e212081dcbbdf49958a1dfdd9
Roberto <roberto.quadraccia90@gmail.com>
2024-01-30 14:22:59 +0100

    useInput created and implemented, validation moved inside the file

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,60 +1,34 @@
-import { useState } from "react";
 import Input from "./Input";
 import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState("");
-  // const [enteredPassword, setEnteredPassword] = useState("");
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  // The state management hook used was moved to useInput.js file.
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
 
-  const emailIsInvalid =
-    didEdit.email &&
-    !isEmail(enteredValues.email) &&
-    !isNotEmpty(enteredValues.email);
-  const passwordIsInvalid =
-    didEdit.password && !hasMinLength(enteredValues.password, 6);
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6));
 
-  const handleInputChange = (identifier, value) => {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  };
-
-  const handleInputBlur = (identifier) => {
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: true,
-    }));
-  };
-
-  // const handleEmailChange = (e) => {
-  //   setEnteredEmail(() => e.target.value);
-  // };
-
-  // const handlePasswordChange = (e) => {
-  //   setEnteredPassword(() => e.target.value);
-  // };
+  // handleInputChange and handleInputBlur were moved to useInput.js file.
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("User email: " + enteredEmail);
-    setEnteredValues({
-      email: "",
-      password: "",
-    });
+
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+
+    console.log(emailValue, passwordValue);
   };
 
   return (
@@ -68,10 +42,10 @@ export default function Login() {
           id="email"
           type="email"
           name="email"
-          onBlur={() => handleInputBlur("email")}
-          onChange={(event) => handleInputChange("email", event.target.value)}
-          value={enteredValues.email}
-          error={emailIsInvalid && "Please enter a valid email."}
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Please enter a valid email."}
         />
 
         <Input
@@ -79,12 +53,10 @@ export default function Login() {
           id="password"
           type="password"
           name="password"
-          onBlur={() => handleInputBlur("password")}
-          onChange={(event) =>
-            handleInputChange("password", event.target.value)
-          }
-          value={enteredValues.password}
-          error={passwordIsInvalid && "Please enter a valid password."}
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "Please enter a valid password."}
         />
       </div>
 
---
+++ src/hooks/useInput.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export const useInput = (defaultValue, validationFn) => {
+  // const [enteredValues, setEnteredValues] = useState({
+  //     email: "",
+  //     password: "",
+  //   });
+  //   const [didEdit, setDidEdit] = useState({
+  //     email: false,
+  //     password: false,
+  //   });
+
+  const [enteredValue, setEnteredValue] = useState(defaultValue);
+  const [didEdit, setDidEdit] = useState(false);
+
+  const valueIsValid = validationFn(enteredValue)
+
+  const handleInputChange = (event) => {
+    setEnteredValue(event.target.value);
+    setDidEdit(false);
+  };
+
+  const handleInputBlur = () => {
+    setDidEdit(true);
+  };
+
+  return {
+    value: enteredValue,
+    handleInputChange,
+    handleInputBlur,
+    hasError: didEdit && !valueIsValid
+  };
+};
